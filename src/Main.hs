762147df3b42acd0500af36a8bdc7c40052c5e60
Enzo Lebrun <enzo.the@gmail.com>
2017-10-25 22:55:51 +0200

    generation brute force des graph possible

--- src/Main.hs
+++ src/Main.hs
@@ -2,30 +2,30 @@ module Main where
   import Data.Graph
   import Data.Map (Map)
   import qualified Data.Map as Map
+  import Control.Monad (replicateM)
 
 
   data Color = Black | White | Red | Blue | Green | Orange | Yellow
   instance Show Color where
-    show White = "White"
-    show Black = "Black"
-    show Red = "Red"
-    show Blue = "Blue"
-    show Green = "Green"
+    show White  = "White"
+    show Black  = "Black"
+    show Red    = "Red"
+    show Blue   = "Blue"
+    show Green  = "Green"
     show Orange = "Orange"
     show Yellow = "Yellow"
   instance Eq Color where
     c1 == c2 = (show c1) == (show c2)
-  data ColorGraph = ColorGraph Graph (Map Int Color)
+  data ColorGraph = ColorGraph Graph (Map Int Color) deriving (Show)
 
-  arr = [(0, [1, 2, 5]), (1, [0, 3]), (2, [0, 5]), (3, [1, 4]), (4, [3, 5]), (5, [0, 4])]
+  arr = [(0, [1, 2]), (1, [0, 3]), (2, [0, 5]), (3, [1, 4]), (4, [3, 5]), (5, [4])]
 
   main = do
     let colors = Map.unions [ Map.insert x White (Map.fromList [])| x <- [0..5]]
     let graph = buildG (0, 5) $concat [[(a, x)|x<-b]|(a, b)<-arr]
-    let colorGraph = (ColorGraph graph colors)
-    putStrLn $show $Map.toList colors
-    putStrLn $show $checkNeighbors 0 graph
-    putStrLn $show $validGraph colorGraph
+    let kColorsGraphs = filter validGraph $generateAllKColorGraphs graph (length $vertices graph) 3
+    putStrLn $show kColorsGraphs
+
 
   checkNeighbors :: Int -> Graph -> [Int]
   checkNeighbors x graph =
@@ -33,13 +33,31 @@ module Main where
 
   validGraph :: ColorGraph -> Bool
   validGraph (ColorGraph graph colors) =
-    length(concat [
-      [
-        Map.lookup n colors == Map.lookup v colors
-          | n <- (checkNeighbors v graph)
-      ]
-        | v <- (vertices graph)
-        ]) == 0
+    length(
+        concat [[Map.lookup n colors==Map.lookup v colors
+                    |n<-(checkNeighbors v graph)]
+                        |v<-(vertices graph)]
+                        ) == 0
+
+  generateAllKColorGraphs :: Graph -> Int -> Int -> [ColorGraph]
+  generateAllKColorGraphs graph i k = generateAllGraphs graph i (take k [White, Black, Red, Blue, Green, Orange, Yellow])
+
+  generateAllGraphs :: Graph -> Int -> [Color] -> [ColorGraph]
+  generateAllGraphs graph i colors =
+    map (ColorGraph graph)
+      $map (Map.fromList)
+        $map (zip [0..i])
+          $replicateM i colors
+
+
+
+
+
+
+
+
+
+
 
 
 
